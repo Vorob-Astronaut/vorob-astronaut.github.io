70c1510f30d5fad4d1e2561b052339c4436a685e
Vorob-Astronaut <e.vorob8@gmail.com>
2024-01-20 01:06:15 +0000

    Deploying to gh-pages from @ Vorob-Astronaut/vorob-astronaut.github.io@5ca117d6f61885b1d0574cfd9b8daa7bd1d254da 🚀

---
+++ _next/static/6mmUqiL-hG3hYCjnMnaM-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-d8ca80a63ecc2b49.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/chunks/pages/_app-238a0c9c3ca62ca4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{7334:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(9331)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(7334),_(3172)}),_N_E=n.O()}]);

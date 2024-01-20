e26b299f4073949012acf14b59f79a7de88bafa0
egor <egor@egors-MacBook-Pro.local>
2024-01-20 02:15:48 +0100

    Change

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  output: 'export',
-  assetPrefix: process.env.ASSET_PREFIX,
-  basePath: process.env.BASE_PATH,
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
